afc56722337bda141f7486c024de180738947ac7
Egor Kornev <boolgater@gmail.com>
2023-04-07 19:59:38 +0300

    Remove unused code

--- src/main/java/org/hse/parkings/config/ClockConfig.java
+++ src/main/java/org/hse/parkings/config/ClockConfig.java
@@ -1,10 +1,8 @@
 package org.hse.parkings.config;
 
 import org.hse.parkings.utils.DateTimeProvider;
-import org.springframework.boot.validation.MessageInterpolatorFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.time.Clock;
 
@@ -19,20 +17,5 @@ public class ClockConfig {
     @Bean
     public Clock getClock() {
         return DateTimeProvider.getInstance().getClock();
-    }
-
-    @Bean
-    public LocalValidatorFactoryBean defaultValidator(Clock clock) {
-        LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean() {
-
-            @Override
-            protected void postProcessConfiguration(javax.validation.Configuration<?> configuration) {
-                configuration.clockProvider(() -> clock);
-            }
-        };
-        MessageInterpolatorFactory interpolatorFactory =
-                new MessageInterpolatorFactory();
-        factoryBean.setMessageInterpolator(interpolatorFactory.getObject());
-        return factoryBean;
     }
 }
